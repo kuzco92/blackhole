03e88784db0f6be88547ac043e427b8a05991bd9
dave <jezreldavesondia@gmail.com>
2020-10-16 10:00:18 +0800

    10-16

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -150,6 +150,117 @@
           매일 푸쉬
         </v-card>
       </div>
+      <div class="text-center mt-10 h5 nanum">
+        1회 수업 : 회당 40분 이상 소요 / 한 과정당 3개월~6개월 소요
+      </div>
+    </v-container>
+    <v-container class="px-0 py-0 mt-10">
+      <v-img src="../assets/level_bar.png"></v-img>
+      <v-row justify="space-around">
+        <v-col cols="6" md="2">
+          <div>1 LEVEL</div>
+          <div class="mt-5">
+            미취학, 초등저학년 <br />
+            /이미지 연상법
+          </div>
+          <div class="caption-text mt-5">
+            * 주요 기본단어 10개씩 암기 <br />
+            * 듣기. 말하기 수업
+          </div>
+        </v-col>
+        <v-col cols="6" md="2">
+          <div>2 LEVEL</div>
+          <div class="mt-5">초등 1~2학년</div>
+          <div class="caption-text mt-5">
+            * 주요 기본단어 20개씩 암기 * 듣기. 말하기 수업
+          </div>
+        </v-col>
+        <v-col cols="6" md="2">
+          <div>3 LEVEL</div>
+          <div class="mt-5">
+            초등 5~ 중등 2학년 <br />
+            /영어로 습득하는 배경지식
+          </div>
+          <div class="caption-text mt-5">
+            * 주요 기본단어 30개씩 암기 <br />
+            * 논리적인 사고를 유도하는 독해훈련
+          </div>
+        </v-col>
+        <v-col cols="6" md="2">
+          <div>4 LEVEL</div>
+          <div class="mt-5">
+            중등 1~ 고등 1학년 <br />
+            /영어교과서 기반
+          </div>
+          <div class="caption-text mt-5">
+            * 하루 단어 40개씩 <br />
+            * 발음교정과 독해훈련 <br />
+            * 문장구조 이해력 강화
+          </div>
+        </v-col>
+        <v-col cols="6" md="2">
+          <div>5 LEVEL</div>
+          <div class="mt-5">고등 1~3학년 /수능독해를 위한 문제</div>
+          <div class="caption-text mt-5">
+            * 하루 단어 50개씩 <br />
+            * 암기 발음교정과 듣기연습 <br />
+            * 영어로 습득하는 최신시사 <br />
+            (경제 / 국제사회 뉴스 활용) <br />
+            ▶ 배경 지식 확장
+          </div>
+        </v-col>
+      </v-row>
+      <div class="d-flex mx-auto">
+        <v-btn
+          class="mx-auto py-5 px-10 rounded-tl-xl rounded-tr-xl rounded-bl-xl white--text mb-10"
+          x-large
+          depressed
+          color="#34163e"
+        >
+          <span class="h5 nanum">블랙홀 무료체험</span>
+        </v-btn>
+      </div>
+    </v-container>
+    <v-divider></v-divider>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" md="5">
+          <div class="h4 gmarket text-center text-purple">영자신문 활용</div>
+          <v-img src="../assets/img-news.png" height="80%"></v-img>
+          <div>
+            * 문장을 통해 중요단어 파악 <br />
+            * 문장속에서 의미 파악 <br />
+            * 외국인 강사 액센트 지도
+          </div>
+        </v-col>
+        <v-divider vertical></v-divider>
+        <v-col cols="12" md="5">
+          <div class="h4 gmarket text-center text-purple">효과적인 암기법</div>
+          <div class="text-center mt-5">
+            영어학습에서 가장 중요한건? 단어암기!
+          </div>
+          <v-img src="../assets/img-graph.png" class="my-10"></v-img>
+          <div class="mt-10">
+            <span class="text-purple font-weight-black"> 암기 3단계 :</span>
+            암기 – 기억인출 – 철자확인 <br />
+            <span class="text-purple font-weight-black">확인 2단계 :</span>
+            테스트 , 스피드퀴즈 <br />
+            <span class="text-purple font-weight-black">복습 5단계 :</span> 3일,
+            4일, 5일, 7일, 30일 반복테스트
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container fluid class="px-0 py-0">
+      <v-card tile color="#e6ddf8">
+        <div class="text-center h3 gmarket">많은 학부모님들이 인정한</div>
+        <div class="text-center h3 gmarket">
+          단어암기 끝장 솔루션! 블랙홀과 함께해요~
+        </div>
+        <v-container>
+          <v-row> </v-row>
+        </v-container>
+      </v-card>
     </v-container>
   </v-app>
 </template>
